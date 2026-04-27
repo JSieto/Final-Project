f47d714b7ee6ad50a852bd8d5dc7401354037367
Josh <jqs7370@psu.edu>
2026-04-27 17:44:50 -0400

    commit_two

--- Project.py
+++ Project.py
@@ -9,14 +9,30 @@ class Number_Game:
     def generate_number(self,difficulty):
         if difficulty == "easy":
             self.number = random.randint(1, 100)
+            print("easy number generated")
         elif difficulty == "medium":
             self.number = random.randint(1, 1000)
+            print("medium number generated")
         elif difficulty == "hard":
             self.number = random.randint(1, 10000)
+            print("hard number generated")
         else:
             print("Please enter a valid difficulty")
 
     def guess(self,guess):
         correct = False
+        
+        if not int(guess):
+            print ("please enter a valid guess")
 
-        ##while correct is False:
+        while correct is False:
+            if guess > self.number:
+                print("guess is too large")
+                self.num_guesses +=1
+            elif guess < self.number:
+                print("guess is too small")
+                self.num_guesses +=1
+            else:
+                print("you guessed the number")
+                self.num_guesses +=1
+            
